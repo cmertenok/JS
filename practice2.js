5ec6fb782bb8a66bcc7e19ad4b3f6dbd890cc561
cmertenok <artempozitiv56@gmail.com>
2020-12-01 01:13:55 +0200

    Practice

--- practice2.js
+++ practice2.js
@@ -1,3 +1,17 @@
-'use strict';
+'use strict'
+const log = (base, n) => Math.log(n) / Math.log(base);
+const createlog = base => n => log(base, n);
 
-console.log('SUCK')
+const lg = createlog(10);
+const ln = createlog(Math.E);
+
+function getE(){
+    return Math.E;
+}
+function getPi(){
+    return Math.PI;
+}
+console.log('Log(5) = ' + lg(5));
+console.log('ln(10) = ' + ln(5));
+console.log('E = ' + getE());
+console.log('PI = ' + getPi());
