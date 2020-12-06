e3cd9316c4a98a50e2c3521abcccfe0039e79099
cmertenok <artempozitiv56@gmail.com>
2020-12-06 21:40:40 +0200

    Update Practice2.1.js

--- Practice2.1.js
+++ Practice2.1.js
@@ -10,10 +10,12 @@ function key() {
     return text;
   }
   
-  console.log(`1) ${key()}-${key()}-${key()}-${key()}`);
-  console.log(`2) ${key()}-${key()}-${key()}-${key()}`);
-  console.log(`3) ${key()}-${key()}-${key()}-${key()}`);
-  console.log(`4) ${key()}-${key()}-${key()}-${key()}`);
-  console.log(`5) ${key()}-${key()}-${key()}-${key()}`);
+  console.log(`${key()}-${key()}-${key()}-${key()}`);
+  console.log(`${key()}-${key()}-${key()}-${key()}`);
+  console.log(`${key()}-${key()}-${key()}-${key()}`);
+  console.log(`${key()}-${key()}-${key()}-${key()}`);
+  console.log(`${key()}-${key()}-${key()}-${key()}`);
+
+  
 
   
