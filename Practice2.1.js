1545e06902d00c8efac845c94f48f44e63bac839
cmertenok <artempozitiv56@gmail.com>
2020-12-10 20:35:38 +0200

    Update Practice2.1.js

--- Practice2.1.js
+++ Practice2.1.js
@@ -1,6 +1,6 @@
 'use strict'
 
-function key() {
+/*function key() {
     let text = "";
     let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   
@@ -14,8 +14,37 @@ function key() {
   console.log(`${key()}-${key()}-${key()}-${key()}`);
   console.log(`${key()}-${key()}-${key()}-${key()}`);
   console.log(`${key()}-${key()}-${key()}-${key()}`);
-  console.log(`${key()}-${key()}-${key()}-${key()}`);
+  console.log(`${key()}-${key()}-${key()}-${key()}`);*/
 
-  
+  /*function range(start, end) {
+    let array = [];
+    for(start; start <= end; start++){
+      array.push(start);
+    }
+    console.dir({range: array});
 
-  
+  }
+
+  range(15,30);*/
+
+  /*function sum(a,b){
+    return a / b;
+  }
+  console.log(sum(25,25))*/
+
+  /*let array = [5,6,7,7,8,'lov',8,8,9,9,0,0,0,0];
+  //console.log(array.length);
+  array.push(19, 'lox');
+  array.unshift(10, 'hf');
+  console.log(array);
+  console.log(array.length);*/
+//const power = Math.pow;
+  //const sqr = x => power(x, 2);
+//  console.log(power(10,6));
+
+/*let admin, name;
+
+name = 'Jon';
+
+admin = name;
+console.log(admin);*/
