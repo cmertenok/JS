dfe1414a817f0732cf4d76beea57f05d4fa29eb2
cmertenok <artempozitiv56@gmail.com>
2020-12-11 21:03:38 +0200

    Update Practice2.1.js

--- Practice2.1.js
+++ Practice2.1.js
@@ -14,37 +14,31 @@
   console.log(`${key()}-${key()}-${key()}-${key()}`);
   console.log(`${key()}-${key()}-${key()}-${key()}`);
   console.log(`${key()}-${key()}-${key()}-${key()}`);
+  console.log(`${key()}-${key()}-${key()}-${key()}`);
   console.log(`${key()}-${key()}-${key()}-${key()}`);*/
 
-  /*function range(start, end) {
-    let array = [];
-    for(start; start <= end; start++){
-      array.push(start);
-    }
-    console.dir({range: array});
+/*for( let i = 0; i < 10; i++){
 
+  if( i % 2 == 0)continue;
+  console.log(i)
+}*/
+//for(let i = 0; i <= 10; i++){
+  //if( i % 2 == 0)
+ // console.log(i)
+//}
+/*let i = 0;
+while( i < 3){
+  console.log(`Your number is ${i++}`)
+}*/
+
+//let n = 10;
+
+number:
+for (let i = 2; i <= n; i++) { 
+
+  for (let j = 2; j < i; j++) { 
+    if (i % j == 0) continue number; 
   }
 
-  range(15,30);*/
-
-  /*function sum(a,b){
-    return a / b;
-  }
-  console.log(sum(25,25))*/
-
-  /*let array = [5,6,7,7,8,'lov',8,8,9,9,0,0,0,0];
-  //console.log(array.length);
-  array.push(19, 'lox');
-  array.unshift(10, 'hf');
-  console.log(array);
-  console.log(array.length);*/
-//const power = Math.pow;
-  //const sqr = x => power(x, 2);
-//  console.log(power(10,6));
-
-/*let admin, name;
-
-name = 'Jon';
-
-admin = name;
-console.log(admin);*/
+  console.log( i ); 
+}
