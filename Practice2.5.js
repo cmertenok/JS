ca8fc201b88442ed749ad4936fea95bde2db6069
cmertenok <artempozitiv56@gmail.com>
2020-12-15 20:22:15 +0200

    Create Practice2.5.js

---
+++ Practice2.5.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function random(max) {
+    //for( let i = 0; i <= 10; i++)
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  console.log(random(30));
+  console.log('Hello! '.repeat(5))
+
+
+  
+
+  
