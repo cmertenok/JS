ff0e06962da52848190d61801f9a2611b3d2b1c7
cmertenok <artempozitiv56@gmail.com>
2020-12-31 20:03:11 +0200

    Update Practice2.7.js

--- Practice2.7.js
+++ Practice2.7.js
@@ -1,7 +1,12 @@
 'use strict';
 
-let name = 'Jhon';
+/*let name = 'Jhon';
 let admin;
 admin = name;
 console.log(admin);
-console.log(typeof 10n);
+console.log(typeof 10n);*/
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 == 0) {
+      console.log( i );
+    }
+  }
