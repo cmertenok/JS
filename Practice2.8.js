def515cef56097c6ac0d99d59e95b119fa59033d
cmertenok <artempozitiv56@gmail.com>
2021-01-03 16:24:46 +0200

    Update Practice2.8.js

--- Practice2.8.js
+++ Practice2.8.js
@@ -1,17 +1,21 @@
-'use strict';
+'use strict'
 
-let obj = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-  };
-
-  function multiplyNumeric(obj) {
+function multiplyNumeric(obj) {
     for (let key in obj) {
-      if (typeof obj[key] == 'number') {
-        obj[key] *= 2;
-      }
+        if (typeof obj[key]=='number') {
+            obj[key]*=2;
+        }
     }
-  }
+}
 
-  console.log(obj);
+let menu = {
+width: 500,
+height: 1000,
+title: "My menu",
+};
+
+multiplyNumeric(menu);
+
+for (let key in menu) {
+    console.log(menu[key]);
+}
