df09ed2c6c2e9807a1aa0960438c55c6cbea76a2
cmertenok <artempozitiv56@gmail.com>
2021-01-03 16:22:37 +0200

    Update Practice2.8.js

--- Practice2.8.js
+++ Practice2.8.js
@@ -1,2 +1,17 @@
 'use strict';
 
+let obj = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+  function multiplyNumeric(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] == 'number') {
+        obj[key] *= 2;
+      }
+    }
+  }
+
+  console.log(obj);
