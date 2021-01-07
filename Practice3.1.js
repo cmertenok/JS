9ed57d3d55e9a4542e1d072fd01f94094b4d2c59
cmertenok <artempozitiv56@gmail.com>
2021-01-07 22:01:01 +0200

    Update Practice3.1.js

--- Practice3.1.js
+++ Practice3.1.js
@@ -1,11 +1,2 @@
 'use strict';
 
-let user = {
-    name: "Вася"
-  };
-  
-  let id = Symbol("id");
-  
-  user[id] = 1;
-  
-  console.log( user[id] ); // мы можем получить доступ к данным по ключу-символу
