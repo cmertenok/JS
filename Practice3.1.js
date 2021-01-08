5bc493db77f3263625040407c88bb7f1f54c71d2
cmertenok <artempozitiv56@gmail.com>
2021-01-08 20:35:59 +0200

    Update Practice3.1.js

--- Practice3.1.js
+++ Practice3.1.js
@@ -1,2 +1,19 @@
 'use strict';
 
+function isPalidrome(str){
+    let strLen = str.length;
+    let result = '';
+    for(let i = 0; i < strLen; i++){
+        if(str[i] === str[strLen - 1 - i]){
+            result = 'Yes';
+        } else {
+            result = 'No';
+        }
+    }
+    return result;
+}
+
+let test1 = isPalidrome('abcdedcba');
+let test2 = isPalidrome('abcded');
+
+console.log(`Result: \n1)${test1} \n2)${test2}`)
