d4e9115c6df266bde6188f52c6d4017611c87f23
Artem <artempozitiv56@gmail.com>
2021-06-03 21:17:32 +0300

    Cleaned up the code

---
+++ Kurs4ch-main/js/btnTheme.js
@@ -0,0 +1,13 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const switcher = document.querySelector('.btnTheme');
+
+  switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+    const className = document.body.className;
+    className === 'light-theme' ?
+      (this.textContent = 'Dark') :
+      (this.textContent = 'Light');
+  });
+});
